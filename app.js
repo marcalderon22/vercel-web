e877f0d58166587bc41c9fcd3328579524613daf
marcalderon22 <140531153+marcalderon22@users.noreply.github.com>
2023-11-07 14:27:55 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+// Este es un ejemplo simple de JavaScript que muestra una alerta cuando se carga la página.
+document.addEventListener('DOMContentLoaded', function () {
+    alert('¡La página se ha cargado correctamente!');
+  });
+  
