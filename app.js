82398389c84b23f531e9ab8890d3ec323dbb737c
marcalderon22 <140531153+marcalderon22@users.noreply.github.com>
2023-11-08 09:54:53 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+let count = 0;
+var dastDictionary = {
+    "injection": "It is a type of attack that is carried out through user data input.",
+    "XSS": "It is a type of attack that injects malicious code into web pages viewed by other users.",
+    // Agrega más palabras y definiciones aquí
+   };
+
 function myFunction() {
-    alert('DAST tests are performed on CircleCI in the dast branch and on Probely');
+    count++;
+    document.getElementById('countLabel').textContent = "Número de veces presionado: " + count;
+    var inputValue = document.getElementById('countInput').value;
+    if (inputValue in dastDictionary) {
+        alert(dastDictionary[inputValue]);
+    } else {
+        alert('DAST tests are performed on CircleCI in the dast branch and on Probely');
+    }
 }
